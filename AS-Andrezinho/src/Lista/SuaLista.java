fb061d2290b6756e9a5a2af5b4f2ac40e9425823
higor <higor@DESKTOP-TMNINGF.local>
2023-09-16 21:23:23 -0300

    16/09 - 2

--- AS-Andrezinho/src/Lista/SuaLista.java
+++ AS-Andrezinho/src/Lista/SuaLista.java
@@ -4,5 +4,7 @@ import java.util.ArrayList;
 
 public interface SuaLista<E>{
     public void exibirLista(ArrayList<?> lista);
+    public void ordenarLista(ArrayList<E> lista);
     public E pesquisar(String chaveBusca);
+    public void filtrar(String chaveBusca);
 }
