06f45eb458d8e4c19c61bfc1b16267cd3464aef1
HigorPMorais <higorpinto901@gmail.com>
2023-09-22 20:12:19 -0300

    22/09 - 1

--- AS-Andrezinho/src/Lista/SuaLista.java
+++ AS-Andrezinho/src/Lista/SuaLista.java
@@ -1,10 +1,14 @@
 package Lista;
 
-import java.util.ArrayList;
+import java.util.List;
 
-public interface SuaLista<E>{
-    public void exibirLista(ArrayList<?> lista);
-    public void ordenarLista(ArrayList<E> lista);
+public interface SuaLista<E> extends List{
+    public void exibirLista();
+    public void exibirListaOrdenada();
+    public void ordenar();
+    public void ordenarDecrescente();
     public E pesquisar(String chaveBusca);
-    public void filtrar(String chaveBusca);
+    public E pesquisarPorVariasChaves(String chaveBusca);
+    public ListaGenerica<?> filtrar(String chaveBusca);
+    public ListaGenerica<?> filtrarPorVariasChaves(String chaveBusca);
 }
